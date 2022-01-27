91f35ede1ab1bcea71ab2962312765bf5eb37a72
Gary <gary_markoski@hotmail.com>
2022-01-27 16:59:49 -0700

    added navigation.spec and appointment.spec

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,32 +1,36 @@
-import React from 'react';
-import InterviewerListItem from 'components/InterviewerListItem';
+import React, { useState } from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 import PropTypes from 'prop-types';
-import 'components/InterviewerList.scss';
 
 
 
 InterviewerList.propTypes = {
-  interviewer: PropTypes.number,
-  onChange: PropTypes.func.isRequired
+  interviewers: PropTypes.array.isRequired
 };
-
 export default function InterviewerList(props) {
-  
-  let interviewers = props.interviewers.map(interviewer => {
-    return (
-      <InterviewerListItem
-        key={interviewer.id}
-        name={interviewer.name}
-        avatar={interviewer.avatar}
-        selected={interviewer.id === props.value}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
-      />
-    );
-  });
-  return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{interviewers}</ul>
-    </section>
-  );
-}
+
+  const [value, setValue] = useState(props.value);
+  const onChange = (id) => {
+    props.onChange(id)
+    console.log(id) 
+    setValue(id);
+  }
+  const interviewers = props.interviewers.map((interviewer) => 
+
+  <InterviewerListItem 
+    key= {interviewer.id}
+    name= {interviewer.name}
+    avatar= {interviewer.avatar}
+    selected= {interviewer.id === props.value}
+    setInterviewer={() => onChange(interviewer.id)}
+  />
+  )
+ 
+  return <section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewers</h4>
+  <ul className="interviewers__list">{interviewers}</ul>
+</section>
+
+
+} 
