8bac29ae93f0f19ef16d0ecf44e907c2d24402f8
Gary <gary_markoski@hotmail.com>
2022-01-30 13:56:11 -0700

    correcting bug

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,42 +1,62 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
-
   const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
+    day: "Monday", // The day string is a direct child of the state object
+    days: [], // The days array is a direct child of the state object
+    appointments: {}, // This object is a direct child of the state object, originally set to null it will set to an object when we book an interview
+    interviewers: {}, // This object is a direct child of the state object
+    cancelInterview: {},
   });
 
-  const setDay = (day) => setState((prev) => ({ ...prev, day }));
+  const setDay = (day) => setState((prev) => ({ ...prev, day })); // added this as part of the separation of concerns refactor
 
-  const bookInterview = (id, interview) => {
+  // Intital request to get all the data from the three endpoints, days, appointments, interviewers
+  useEffect(() => {
+    const fetchDays = axios.get("http://localhost:8001/api/days");
+    const fetchAppointments = axios.get(
+      "http://localhost:8001/api/appointments"
+    );
+    const fetchInterviewers = axios.get(
+      "http://localhost:8001/api/interviewers"
+    );
+
+    Promise.all([
+      Promise.resolve(fetchDays),
+      Promise.resolve(fetchAppointments),
+      Promise.resolve(fetchInterviewers),
+    ])
+      .then((response) => {
+        setState((prev) => ({
+          ...prev,
+          days: response[0].data,
+          appointments: response[1].data,
+          interviewers: response[2].data,
+        }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  /////----- book interview -----/////
+  function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview }
+      interview: { ...interview },
     };
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
+    const days = copyDayState(state.days, appointments); // this is update spots
 
-    setState({ ...state, appointments });
-
-    const days = copyDayState(state.days, appointments);
-
-    return axios.put(`/api/appointments/${id}`, appointment) // send the new appointment info to the server
-      .then((res) => {
-        setState({
-          ...state,
-          appointments,
-          days
-        })
-      })
-      .catch((err) => {
-        console.log("Error message: ", err)
-      })
+    return axios
+      .put(`/api/appointments/${id}`, appointment) // send the new appointment info to the server
+      .then(() => {
+        setState({ ...state, appointments, days }); //updateSpots is linked via the last variable here
+      });
   }
 
   /////----- delete -----/////
@@ -49,63 +69,32 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment,
     };
-
-    const days = copyDayState(state.days, appointments);
+    const days = copyDayState(state.days, appointments); // this is update spots
 
     return axios
       .delete(`/api/appointments/${id}`) // send the delete id request to the server
-      .then((res) => {
-        setState({ ...state, appointments, days });
-      })
-      .catch((err) => {
-        console.log("Error message: ", err)
+      .then(() => {
+        setState({ ...state, appointments, days }); //updateSpots is last variable here
       });
   }
-  useEffect(() => {
 
-    const fetchDays = axios.get('/api/days');
-    const fetchAppointments = axios.get('/api/appointments');
-    const fetchInterviewers = axios.get('/api/interviewers');
-
-    Promise.all([
-      Promise.resolve(fetchDays),
-      Promise.resolve(fetchAppointments),
-      Promise.resolve(fetchInterviewers)
-
-    ])
-      .then((response) => {
-        setState(prev => ({
-          ...prev,
-          days: response[0].data,
-          appointments: response[1].data,
-          interviewers: response[2].data
-        }));
-
-      });
-  }, []);
-
+  /////----- update spots -----/////
   function updateSpots(day, appointments) {
-    let counter = 0
+    let counter = 0;
     day.appointments.forEach((id) => {
       if (appointments[id].interview === null) {
-        counter++
+        counter++;
       }
-
-    })
+    });
     return counter;
   }
 
   function copyDayState(days, appointments) {
     const dayArray = days.map((day) => {
       return { ...day, spots: updateSpots(day, appointments) };
-    })
-    return dayArray
+    });
+    return dayArray;
   }
 
-  return {
-    state,
-    setDay,
-    bookInterview,
-    cancelInterview
-  }
-};
+  return { state, setState, setDay, bookInterview, cancelInterview };
+}
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,18 +5,17 @@ export default function useVisualMode(initial) {
 
   const [history, setHistory] = useState([initial]);
 
-  const back = () => {
-    if (history.length > 1) {
-      const newHistory = [...history.slice(0, history.length - 1)];
-      setHistory(newHistory);
-      setMode(newHistory[newHistory.length - 1]);
-    }
-  };
-
   const transition = (newMode, replace = false) => {
     if (replace) back();
     setHistory((prev) => [...prev, newMode]);
     return setMode(newMode);
+  };
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = [...history.slice(0, history.length - 1)]; // alternate way is to use push and pop
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
   };
 
   return { mode, transition, back };
