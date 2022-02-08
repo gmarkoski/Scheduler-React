d40499f26fdb06d33013ba56819a9932d35947c6
Gary <gary_markoski@hotmail.com>
2022-02-08 14:43:08 -0700

    fixed proper file in file conflict

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,20 +1,14 @@
-import React from 'react';
+import React from "react";
 
-function EmptyTest(props) {
+export default function Empty(props) {
   return (
-
     <main className="appointment__add">
-      <h2>Hiya</h2>
       <img
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
         onClick={props.onAdd}
-      
       />
     </main>
-
   );
 }
-
-export default EmptyTest;
